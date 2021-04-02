7b091b58f991feb187ea9603d52433db68126aff
Piotr Pawełczak <pawelczakpiotr99@gmail.com>
2021-04-02 17:08:49 +0200

    get_absolute_url method in Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -29,3 +30,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        post_year = self.publish.year
+        post_month = self.publish.strftime('%m')
+        post_day = self.publish.strftime('%d')
+        post_slug = self.slug
+        return reverse('blog:post_detail', args=[post_year, post_month, post_day, post_slug])
