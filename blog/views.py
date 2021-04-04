ea9956d91765e972c46d51b649737b8bd0648480
Piotr Pawełczak <pawelczakpiotr99@gmail.com>
2021-04-04 13:23:54 +0200

    Add sharing post by e-mail

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -24,3 +20,25 @@ def post_detail(request, year, month, day, post):
                              publish__year=year, publish__month=month, publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            # subject = f'{cd["name"]} ({cd["email"]}) zachęca do przeczytania "{post.title}"'
+
+            subject = f"{cd['name']} ({cd['email']}) zacheca do przeczytania \"{post.title}\" "
+            message = f'Przeczytaj post "{post.title}" na stronie {post_url}\n\nKomentarz dodany przez {cd["name"]}: {cd["comments"]}'
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
