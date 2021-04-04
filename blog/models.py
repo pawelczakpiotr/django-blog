c934d1ca6a5a9c0b1a5ad919b4c6ccf713ba59cb
Piotr Pawełczak <pawelczakpiotr99@gmail.com>
2021-04-04 14:31:40 +0200

    Add comments functionality

--- blog/models.py
+++ blog/models.py
@@ -37,3 +37,21 @@ class Post(models.Model):
         post_day = self.publish.strftime('%d')
         post_slug = self.slug
         return reverse('blog:post_detail', args=[post_year, post_month, post_day, post_slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Komentarz dodany przez {self.name} dla posta {self.post}'
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from blog.models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 # Create your views here.
 
 
 class PostListView(ListView):
-    model = Post
+    queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
@@ -19,7 +19,27 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month, publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    added = False
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            added = True
+    else:
+        comment_form = CommentForm()
+
+    values_dict = {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form,
+        'added': added,
+    }
+
+    return render(request, 'blog/post/detail.html', values_dict)
 
 
 def post_share(request, post_id):
@@ -31,8 +51,6 @@ def post_share(request, post_id):
         if form.is_valid():
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
-            # subject = f'{cd["name"]} ({cd["email"]}) zachęca do przeczytania "{post.title}"'
-
             subject = f"{cd['name']} ({cd['email']}) zacheca do przeczytania \"{post.title}\" "
             message = f'Przeczytaj post "{post.title}" na stronie {post_url}\n\nKomentarz dodany przez {cd["name"]}: {cd["comments"]}'
             send_mail(subject, message, 'admin@myblog.com', [cd['to']])
@@ -41,4 +59,3 @@ def post_share(request, post_id):
         form = EmailPostForm()
 
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
-
