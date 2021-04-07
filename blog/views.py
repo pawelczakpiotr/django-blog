9bb2bcedc495ce62cb19712e53858cb326fd6736
Piotr Pawełczak <pawelczakpiotr99@gmail.com>
2021-04-07 14:30:33 +0200

    Add templetetags

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,36 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from blog.forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -32,14 +53,20 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    values_dict = {
+    # Lista podonych postów
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:2]
+
+    context_dict = {
         'post': post,
         'comments': comments,
         'comment_form': comment_form,
         'added': added,
+        'similar_posts': similar_posts,
     }
 
-    return render(request, 'blog/post/detail.html', values_dict)
+    return render(request, 'blog/post/detail.html', context_dict)
 
 
 def post_share(request, post_id):
